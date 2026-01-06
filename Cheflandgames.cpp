868db2ead8f2a917ee9da7d80f23ca516c298236
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-01-06 22:17:54 +0530

    Update Cheflandgames.cpp

--- Cheflandgames.cpp
+++ Cheflandgames.cpp
@@ -5,20 +5,20 @@ int main(){
     bool R1, R2, R3, R4;
     
     do{
-//        cout << "\nTest cases: ";
+        cout << "\nTest cases: ";
         cin >> T;
     }while(T < 1 || T > 20);
     
     while(T > 0, T--){
 
-//        cout << "\n1: if in\n0:if out";
-//        cout << "\nRef1: ";
+        cout << "\n1: if in\n0:if out";
+        cout << "\nRef1: ";
         cin >> R1;
-//        cout << "\nRef2: ";
+        cout << "\nRef2: ";
         cin >> R2;
-//        cout << "\nRef3: ";
+        cout << "\nRef3: ";
         cin >> R3;
-//        cout << "\nRef4: ";
+        cout << "\nRef4: ";
         cin >> R4;
         if(R1 == 0 && R2 == 0 && R3 == 0 && R4 == 0){
             cout << "\nIN";
