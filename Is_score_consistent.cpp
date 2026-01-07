fba8036c39e35315c030d6975177d2b5f6c403d6
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-01-08 00:06:02 +0530

    Erroneous code replaced

--- Is_score_consistent.cpp
+++ Is_score_consistent.cpp
@@ -1,15 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T, A, B, C, D;
+    
+    do{
+//        cout << "\nTest cases: ";
+        cin >> T;
+    }while(T < 1 || T > 1000);
+    
+    while(T > 0, T--){
         do{
-            cout << "\nPossible total goals by A: ";
+//            cout << "\nGoals by team A: ";
+            cin >> A;
+        }while(A < 0 || A > 10);
+        do{
+//            cout << "\nGoals by tem B: ";
+            cin >> B;
+        }while(B < 0 || B > 10);
+        do{
+//            cout << "\nPossible total goals by A: ";
             cin >> C;
-        }while(C < 1 || C > 10);
+        }while(C < 0 || C > 10);
         do{
-            cout << "\nPossible goals by team B: ";
+//            cout << "\nPossible goals by team B: ";
             cin >> D;
-        }while(D < 1 || D > 10);
+        }while(D < 0 || D > 10);
         if(A > C || B > D){
-            cout << "IMPOSSIBLE" << endl;
+            cout << endl << "IMPOSSIBLE";
         }else{
-            cout << "POSSIBLE" << endl;
+            cout << endl << "POSSIBLE";
         }
     }
 }
