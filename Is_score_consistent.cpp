9b94e83926ba93aac3fd169e8ba34269f7440ffd
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-01-08 00:06:22 +0530

    Enable user prompts in Is_score_consistent.cpp
    Uncommented output statements for user prompts.

--- Is_score_consistent.cpp
+++ Is_score_consistent.cpp
@@ -4,25 +4,25 @@ int main(){
     int T, A, B, C, D;
     
     do{
-//        cout << "\nTest cases: ";
+        cout << "\nTest cases: ";
         cin >> T;
     }while(T < 1 || T > 1000);
     
     while(T > 0, T--){
         do{
-//            cout << "\nGoals by team A: ";
+            cout << "\nGoals by team A: ";
             cin >> A;
         }while(A < 0 || A > 10);
         do{
-//            cout << "\nGoals by tem B: ";
+            cout << "\nGoals by tem B: ";
             cin >> B;
         }while(B < 0 || B > 10);
         do{
-//            cout << "\nPossible total goals by A: ";
+            cout << "\nPossible total goals by A: ";
             cin >> C;
         }while(C < 0 || C > 10);
         do{
-//            cout << "\nPossible goals by team B: ";
+            cout << "\nPossible goals by team B: ";
             cin >> D;
         }while(D < 0 || D > 10);
         if(A > C || B > D){
