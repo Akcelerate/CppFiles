626af73a44144835c945a2e62337efe89cbfebc0
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-01-13 00:26:11 +0530

    Add input validation for test cases and nibbles
    Implement input validation for test cases and nibbles.

---
+++ GoodProgram.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T, N;
+    
+    do{
+        cout << "\nTest cases: ";
+        cin >> T;
+    }while(T < 1 || T > 1000);
+    
+    while(T--){
+        do{
+            cout << "\nNibbles: ";
+            cin >> N;
+        }while(N < 1 || N > 1000);
+
+      //Incorrect logic; Incorrect output in case: T=1, N=2 ?;  How?
+        if(N/2 > 0 && N%2 == 0){
+            cout << "\nGood";
+        }else{
+            cout << "\nNot Good";
+        }
+    }
+}
