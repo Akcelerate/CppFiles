04a4ce921e98a807d81b6b8e930fa2cdaef49c1d
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-01-24 19:21:05 +0530

    Create Factorial_Smallint.cpp
    This only works for smaller number i.e., up to about 20. For any greater than that, make a multiplication program which multiplies each digit individually, the same way we do by hand, using an array.

---
+++ Factorial_Smallint.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+unsigned long long factorial(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+int main(){
+    int t, n;
+    
+    do{
+        cout << "\nTest cases: ";
+        cin >> t;
+    }while(t < 1 || t > 100);
+    
+    while(t--){
+        do{
+            cout << "\nFactorial of ";
+            cin >> n;
+        }while( n < 1 || n > 100);
+        
+        cout << " = " << factorial(n) << endl;
+    }
+}
