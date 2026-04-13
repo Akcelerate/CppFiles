8a933f6448b09898752ba0a93cba6bf993633a33
Akshat Pawar <akshat.pawar.original@gmail.com>
2026-04-14 02:31:01 +0530

    Fix DNA nucleotide transformation logic
    Corrected logic for DNA nucleotide changes and improved code structure.

--- Compli_DNA_Strand.cpp
+++ Compli_DNA_Strand.cpp
@@ -1,41 +1,35 @@
-//For some reason out of logic operation being executed 
-//G changes to A, but that logic is not specified
-//Specified logic: G change to C
+//Correxted code
+//* Use "" for strings and '' for individual characters
 
 #include<iostream>
 #include<string>
+
 using namespace std;
+
 int main(){
-    int T, N;
+    int T;
+    int N;
     string S;
 
-    cout << "\nTest cases: ";
     cin >> T;
-    
-    while(T--){
-        cout << "\nNo. of nucleotides: "; 
+    while(T > 0){
         cin >> N;
         do{
-            cout << "\nDNA sequence: ";
             cin >> S;
-  //Add condition so that only {A, T, G, C} are allowed in S
         }while(S.length() != N);
         
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N ; i++){
             if(S[i] == 'A'){
                 S[i] = 'T';
-                goto n;
+            }else if(S[i] == 'T'){
+                S[i] = 'A';
             }else if(S[i] == 'C'){
                 S[i] = 'G';
-                goto n;
-            }else if(S[i] = 'T'){
-                S[i] = 'A';
             }else{
                 S[i] = 'C';
             }
-            n:;
         }
-        
         cout << S << endl;
+        T--;
     }
 }
